19be5c39dc2879d3f4adc408c8d60066b4e7cedc
Ahmed Nour Eldin <44178039+ahmednreldin@users.noreply.github.com>
2021-09-21 15:36:07 +0200

    code clean up

--- script.py
+++ script.py
@@ -8,22 +8,22 @@ print( "Change directory to /kaggle")
 os.chdir(root_path)
 
 print( "install gdown package") 
-conda install -y gdown ; 
+os.system('conda install -y gdown') 
 
-gdown --id  1JtXz4q0kONNoEFE9tEfSCxLRuuJUWqIF # CVM-NET Model ; 
-unzip CVM-Net_model.zip
+os.system('gdown --id  1JtXz4q0kONNoEFE9tEfSCxLRuuJUWqIF # CVM-NET Model') 
+os.system('unzip CVM-Net_model.zip')
 
 os.chdir(working_path)
 
-git clone https://github.com/ahmednreldin/crossview_localisation # clone repo
-mv '/kaggle/CVM-Net_model/CVM-NET-I/CVM-Net-I_init/' '/kaggle/working/crossview_localisation/src/Model/CVM-NET-I'
+os.system('git clone https://github.com/ahmednreldin/crossview_localisation # clone repo')
+os.system('mv '/kaggle/CVM-Net_model/CVM-NET-I/CVM-Net-I_init/' '/kaggle/working/crossview_localisation/src/Model/CVM-NET-I'')
 #rename model init to 0 
-mv /kaggle/crossview_localisation/src/Model/CVM-NET-I/CVM-Net-I_init /kaggle/crossview_localisation/src/Model/CVM-NET-I/0
-cp -r  /kaggle/input/cvusa-localization /kaggle/working
+os.system('mv /kaggle/crossview_localisation/src/Model/CVM-NET-I/CVM-Net-I_init /kaggle/crossview_localisation/src/Model/CVM-NET-I/0')
+os.system('cp -r  /kaggle/input/cvusa-localization /kaggle/working')
 
 # Modify Data 
-mv /kaggle/working/cvusa-localization/splits/splits/* /kaggle/working/cvusa-localization/splits/
-mv /kaggle/working/cvusa-localization/bingmap/bingmap/* /kaggle/working/cvusa-localization/bingmap/
-mv /kaggle/working/cvusa-localization/split_locations/* /kaggle/working/cvusa-localization/
-mv /kaggle/working/cvusa-localization/streetview/streetview/* /kaggle/working/cvusa-localization/streetview/
+os.system('mv /kaggle/working/cvusa-localization/splits/splits/* /kaggle/working/cvusa-localization/splits/')
+os.system('mv /kaggle/working/cvusa-localization/bingmap/bingmap/* /kaggle/working/cvusa-localization/bingmap/')
+os.system('mv /kaggle/working/cvusa-localization/split_locations/* /kaggle/working/cvusa-localization/')
+os.system('mv /kaggle/working/cvusa-localization/streetview/streetview/* /kaggle/working/cvusa-localization/streetview/')
 
