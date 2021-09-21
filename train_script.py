f828f9d54ab228e4d2f24d158b7eab115dd8b1cf
Ahmed Nour Eldin <44178039+ahmednreldin@users.noreply.github.com>
2021-09-21 16:57:46 +0200

    Update train_script.py

--- train_script.py
+++ train_script.py
@@ -2,6 +2,9 @@
 import os
 
 # Run Tran.py
+print("install TF 1.13.2")
 os.system('pip install tensorflow==1.13.2') # install suitable version for TF
 import tensorflow as tf
+
+print("Run train.py")
 os.system('python /kaggle/working/crossview_localisation/src/CVM-Net/train.py' )
