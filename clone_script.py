7170fbdeda42858f48ae9baf20b5d56baf1aba30
Ahmed Nour Eldin <44178039+ahmednreldin@users.noreply.github.com>
2021-09-25 00:32:16 +0200

    Update clone_script.py

--- clone_script.py
+++ clone_script.py
@@ -6,10 +6,10 @@ print("Remove Repo if exist ")
 os.system('rm -r c') 
 
 print("clone localization repo ")
-os.system('!git clone https://github.com/ahmednreldin/crossview_localisation c')
+os.system('git clone https://github.com/ahmednreldin/crossview_localisation c')
 
 print(' Move modified file " ) 
-os.system('!mv c/src/CVM-Net/test_model.py  crossview_localisation/src/CVM-Net/test_model.py')
+os.system('mv c/src/CVM-Net/test_model.py  crossview_localisation/src/CVM-Net/test_model.py')
 
 print('test model')
 os.system('python  crossview_localisation/src/CVM-Net/test_model.py')
