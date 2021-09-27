9010c856896a0d9141b0ae892388fb40dad6fa2d
Ahmed Nour Eldin <44178039+ahmednreldin@users.noreply.github.com>
2021-09-27 18:26:37 +0200

    Update init_script.py

--- init_script.py
+++ init_script.py
@@ -17,14 +17,23 @@ print(f"change directory to {working_path}")
 os.chdir(working_path)
 print("Clone Cross-View Repo")
 os.system('git clone https://github.com/ahmednreldin/crossview_localisation # clone repo')
-print("Copy Init Model to location")
+
+print("Copy Init CVM-NET I Model to location")
 os.system('mv /kaggle/CVM-Net_model/CVM-NET-I/CVM-Net-I_init/ /kaggle/working/crossview_localisation/src/Model/CVM-NET-I')
 #rename model init to 0 
 os.system('mv /kaggle/working/crossview_localisation/src/Model/CVM-NET-I/CVM-Net-I_init /kaggle/working/crossview_localisation/src/Model/CVM-NET-I/0')
 
-print('Copy Pre-trained model to location ')
+print('Copy Pre-trained CVM-NET I model to location ')
 os.system('mv /kaggle/CVM-Net_model/CVM-NET-I/CVM-Net-I_model/* /kaggle/working/crossview_localisation/src/Model/CVM-NET-I/test_model/')
 
+
+
+print("Copy Init CVM-NET II Model to location")
+os.system('mv /kaggle/CVM-Net_model/CVM-NET-II/CVM-Net-II_init/ /kaggle/working/crossview_localisation/src/Model/CVM-NET-II')
+#rename model init to 0 
+os.system('mv /kaggle/working/crossview_localisation/src/Model/CVM-NET-II/CVM-Net-II_init /kaggle/working/crossview_localisation/src/Model/CVM-NET-II/0')
+print('Copy Pre-trained CVM-NET II model to location ')
+os.system('mv /kaggle/CVM-Net_model/CVM-NET-II/CVM-Net-II_model/* /kaggle/working/crossview_localisation/src/Model/CVM-NET-II/test_model/')
 
 # Datasets
 print("Copy dataset from input to working directory..")
